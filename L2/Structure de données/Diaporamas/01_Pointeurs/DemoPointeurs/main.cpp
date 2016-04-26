402aa9c367948f5d20076442e5023d6427c975bd
yvan <yvan.maillot@gmail.com>
2016-04-26 22:53:13 +0200

    Ajout des démos et autres illustrations du cours de SDD sur les pointeurs en L2

---
+++ L2/Structure de données/Diaporamas/01_Pointeurs/DemoPointeurs/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: yvan
+ *
+ * Created on 11 février 2014, 10:39
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  double* p, *q;
+  
+  double  x = 2;
+  
+  p = &x;
+  
+  q = &x;
+
+  return 0;
+}
+
