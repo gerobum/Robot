c4263c937c039365d35398977db0a0398d98fc1f
yvan <yvan.maillot@gmail.com>
2016-09-30 13:06:01 +0200

    Suppression des commentaires du projet ExpressionArithmetique

--- M1/UML/DiagrammesDeClasses/Exercices/Demo/ExpressionArithmetique/src/arithmetique/Division.java
+++ M1/UML/DiagrammesDeClasses/Exercices/Demo/ExpressionArithmetique/src/arithmetique/Division.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arithmetique;
 
-/**
- *
- * @author yvan
- */
 public class Division extends Operation {
 
     public Division(Expression gauche, Expression droite) {
