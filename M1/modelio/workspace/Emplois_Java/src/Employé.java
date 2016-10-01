b2a630b96441525fefe99dfa09f6795e88f1eed0
yvan <yvan.maillot@gmail.com>
2016-10-01 16:42:18 +0200

    Ajout du dossier modelio qui contient les corrections de fascicule sur les diagrammes de classes.

---
+++ M1/modelio/workspace/Emplois_Java/src/Employé.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("8a4dc202-1e38-4780-b914-6a228b9f483f")
+public class Employé {
+    @objid ("552a723c-148f-4adf-a431-85597d943d01")
+    private List<Entreprise> entreprise = new ArrayList<Entreprise> ();
+
+}
---
+++ M1/modelio/workspace/Emplois_Java/src/Entreprise.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("87dcc38d-651d-40e8-8752-92e318ed77e3")
+public class Entreprise {
+    @objid ("27cad2ef-24e9-4f52-851f-acb85cd724ad")
+    private List<Employé> employé = new ArrayList<Employé> ();
+
+}
