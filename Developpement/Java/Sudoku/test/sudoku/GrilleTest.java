fff3225789302f2ae9e3f574b734aac04d333fbb
yvan <yvan.maillot@gmail.com>
2016-10-03 01:46:43 +0200

    Ajout du test du constructeur de grille Mais ça ne marche pas pour l'instant

---
+++ Developpement/Java/Sudoku/test/sudoku/GrilleTest.java
@@ -0,0 +1,59 @@
+package sudoku;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author yvan
+ */
+public class GrilleTest extends TestCase {
+
+    public GrilleTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of getLigne method, of class Grille.
+     */
+    public void testGetLigne() {
+        System.out.println("getLigne");
+
+    }
+
+    public void testConstructeurDeGrille() {
+        System.out.println("Test constructeurde grille");
+        Grille g = new Grille();
+        for (int l = 0; l < 9; ++l) {
+            for (int c = 0; c < 9; ++c) {
+                assertEquals(g.getLigne(l).getCase(c), g.getColonne(c).getCase(l));
+            }
+        }
+    }
+
+    /**
+     * Test of getColonne method, of class Grille.
+     */
+    public void testGetColonne() {
+        System.out.println("getColonne");
+       
+    }
+
+    /**
+     * Test of getCarré method, of class Grille.
+     */
+    public void testGetCarré() {
+        System.out.println("getCarr\u00e9");
+       
+    }
+
+}
