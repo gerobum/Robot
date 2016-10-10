082230ab7e7c643e08b8c730016dc16dc29ee204
Yvan <yvan.maillot@uha.fr>
2016-10-10 14:32:21 +0200

    Des modifications mineurs

--- M2/ComplementsJava/Corrections/04-CalendrierInternational/src/partie1/JLabelCalendar.java
+++ M2/ComplementsJava/Corrections/04-CalendrierInternational/src/partie1/JLabelCalendar.java
@@ -9,8 +9,6 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 
