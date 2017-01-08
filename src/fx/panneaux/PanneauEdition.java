dcbeac19cfbf1b50974c3e4050f2f5887364c956
yvan <yvan.maillot@gmail.com>
2017-01-08 18:46:47 +0100

    Le bouton supprimer a été enlevé. Il est finalement redondant avec couper.

--- src/fx/panneaux/PanneauEdition.java
+++ src/fx/panneaux/PanneauEdition.java
@@ -5,12 +5,9 @@ import fx.programme.instructions.Instruction;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import static fx.utilitaires.Utilitaires.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class PanneauEdition extends PanneauBordure {
 
-    private final Button boutonSupprime = new Button("supprimer");
     private final Button boutonCopie = new Button("copier");
     private final Button boutonColle = new Button("coller");
     private final Button boutonCoupe = new Button("couper");
@@ -28,7 +25,6 @@ public class PanneauEdition extends PanneauBordure {
 
     private void doingUI() {
 
-        add(boutonSupprime, 0, 0);
         add(boutonCopie, 1, 0);
         add(boutonColle, 2, 0);
         add(boutonCoupe, 3, 0);
